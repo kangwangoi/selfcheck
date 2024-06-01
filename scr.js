44304228b5eb92444641267ceb57400da653f443
kangwangoi <153366806+kangwangoi@users.noreply.github.com>
2024-06-01 20:57:48 +0700

    w
    w

---
+++ scr.js
@@ -0,0 +1,28 @@
+var s = 0;
+var m = 0;
+const onclick1 = document.getElementById('button1');
+onclick1.addEventListener('click', (i) => {
+    i.preventDefault();
+    setTimeout(1000)
+document.getElementById('timer1').innerHTML(``)
+    })
+const onclick2 = document.getElementById('button2');
+onclick2.addEventListener('click', (i) => {
+    i.preventDefault();
+
+    })
+const onclick3 = document.getElementById('button3');
+    onclick3.addEventListener('click', (i) => {
+        i.preventDefault();
+    
+        })
+const onclick4 = document.getElementById('button4');
+    onclick4.addEventListener('click', (i) => {
+            i.preventDefault();
+        
+     })
+const onclick5 = document.getElementById('button5');
+     onclick5.addEventListener('click', (i) => {
+         i.preventDefault();
+     
+         })
